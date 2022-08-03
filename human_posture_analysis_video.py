cb731043ba47a9d6974bad0fe8c815465edda3dc
Finger <tangqianjin@healbone.xyz>
2022-08-03 18:15:39 +0800

    feat: add logo

--- human_posture_analysis_video.py
+++ human_posture_analysis_video.py
@@ -2,6 +2,9 @@ import cv2
 import time
 import math as m
 import mediapipe as mp
+import numpy
+import numpy as np
+from PIL import Image, ImageDraw, ImageFont
 
 
 # Calculate distance
@@ -48,7 +51,18 @@ pink = (255, 0, 255)
 # Initialize mediapipe pose class.
 mp_pose = mp.solutions.pose
 pose = mp_pose.Pose()
+
+
 # ===============================================================================================#
+
+def drawText(_image, text, position, color=(255, 255, 255), font_size=32, stroke_width=1):
+    fontpath = "./simsun.ttc"  # <== 这里是宋体路径
+    fontZh = ImageFont.truetype(fontpath, font_size)
+    img_pil = Image.fromarray(_image)
+
+    draw = ImageDraw.Draw(img_pil)
+    draw.text(position, text, font=fontZh, fill=(color[2], color[1], color[0]), stroke_width=stroke_width)
+    return np.array(img_pil)
 
 
 if __name__ == "__main__":
@@ -77,14 +91,8 @@ if __name__ == "__main__":
         # Get height and width.
         h, w = image.shape[:2]
 
-        # Convert the BGR image to RGB.
-        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-
         # Process the image.
         keypoints = pose.process(image)
-
-        # Convert the image back to BGR.
-        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
 
         # Use lm and lmPose as representative of the following methods.
         lm = keypoints.pose_landmarks
@@ -113,9 +121,10 @@ if __name__ == "__main__":
         # Assist to align the camera to point at the side view of the person.
         # Offset threshold 30 is based on results obtained from analysis over 100 samples.
         if offset < 100:
-            cv2.putText(image, str(int(offset)) + ' Aligned', (w - 150, 30), font, 0.9, green, 2)
+            image = drawText(image, str(int(offset)) + ' 对齐', (w - 150, 20), (0, 255, 127), 18)
         else:
-            cv2.putText(image, str(int(offset)) + ' Not Aligned', (w - 150, 30), font, 0.9, red, 2)
+            image = drawText(image, str(int(offset)) + ' 不对齐', (w - 150, 20), (255, 0, 0), 18)
+            # cv2.putText(image, str(int(offset)) + ' Not Aligned', (w - 150, 30), font, 0.9, red, 2)
 
         # Calculate angles.
         neck_inclination = findAngle(l_shldr_x, l_shldr_y, l_ear_x, l_ear_y)
@@ -137,17 +146,17 @@ if __name__ == "__main__":
 
         # Put text, Posture and angle inclination.
         # Text string for display.
-        angle_text_string = 'Neck : ' + str(int(neck_inclination)) + '  Torso : ' + str(int(torso_inclination))
+        angle_text_string = '颈部 : ' + str(int(neck_inclination)) + '°  躯干 : ' + str(int(torso_inclination)) + '°'
 
         # Determine whether good posture or bad posture.
         # The threshold angles have been set based on intuition.
         if neck_inclination < 40 and torso_inclination < 10:
             bad_frames = 0
             good_frames += 1
-            
-            cv2.putText(image, angle_text_string, (10, 30), font, 0.9, light_green, 2)
-            cv2.putText(image, str(int(neck_inclination)), (l_shldr_x + 10, l_shldr_y), font, 0.9, light_green, 2)
-            cv2.putText(image, str(int(torso_inclination)), (l_hip_x + 10, l_hip_y), font, 0.9, light_green, 2)
+
+            image = drawText(image, angle_text_string, (10, 20), (0, 255, 127), 24)
+            image = drawText(image, str(int(neck_inclination)) + "°", (l_shldr_x + 10, l_shldr_y), (0, 255, 127), 24)
+            image = drawText(image, str(int(torso_inclination)) + "°", (l_hip_x + 10, l_hip_y), (0, 255, 127), 24)
 
             # Join landmarks.
             cv2.line(image, (l_shldr_x, l_shldr_y), (l_ear_x, l_ear_y), green, 4)
@@ -159,10 +168,9 @@ if __name__ == "__main__":
             good_frames = 0
             bad_frames += 1
 
-            cv2.putText(image, angle_text_string, (10, 30), font, 0.9, red, 2)
-            cv2.putText(image, str(int(neck_inclination)), (l_shldr_x + 10, l_shldr_y), font, 0.9, red, 2)
-            cv2.putText(image, str(int(torso_inclination)), (l_hip_x + 10, l_hip_y), font, 0.9, red, 2)
-
+            image = drawText(image, angle_text_string, (10, 20), (255, 0, 0), 24)
+            image = drawText(image, str(int(neck_inclination)) + "°", (l_shldr_x + 10, l_shldr_y), (255, 0, 0), 24)
+            image = drawText(image, str(int(torso_inclination)) + "°", (l_hip_x + 10, l_hip_y), (255, 0, 0), 24)
             # Join landmarks.
             cv2.line(image, (l_shldr_x, l_shldr_y), (l_ear_x, l_ear_y), red, 4)
             cv2.line(image, (l_shldr_x, l_shldr_y), (l_shldr_x, l_shldr_y - 100), red, 4)
@@ -171,28 +179,35 @@ if __name__ == "__main__":
 
         # Calculate the time of remaining in a particular posture.
         good_time = (1 / fps) * good_frames
-        bad_time =  (1 / fps) * bad_frames
+        bad_time = (1 / fps) * bad_frames
 
         # Pose time.
         if good_time > 0:
-            time_string_good = 'Good Posture Time : ' + str(round(good_time, 1)) + 's'
-            cv2.putText(image, time_string_good, (10, h - 20), font, 0.9, green, 2)
+            time_string_good = '姿势准确 : ' + str(round(good_time, 1)) + '秒'
+            image = drawText(image, time_string_good, (10, h - 30), (0, 255, 127), 22)
         else:
-            time_string_bad = 'Bad Posture Time : ' + str(round(bad_time, 1)) + 's'
-            cv2.putText(image, time_string_bad, (10, h - 20), font, 0.9, red, 2)
-
-
-
-
+            time_string_bad = '姿势较差 : ' + str(round(bad_time, 1)) + '秒'
+            image = drawText(image, time_string_bad, (10, h - 30), (255, 0, 0), 22)
 
         # If you stay in bad posture for more than 3 minutes (180s) send an alert.
         if bad_time > 180:
             sendWarning()
+
+        logo = cv2.imread('logo.png')
+        width = 123 * 2
+        height = int(width / 4.3)
+        logo = cv2.resize(logo, (width, height))
+        img2gray = cv2.cvtColor(logo, cv2.COLOR_BGR2GRAY)
+        ret, mask = cv2.threshold(img2gray, 1, 255, cv2.THRESH_BINARY)
+        roi = image[-height - 10:-10, -width - 10:-10]
+        roi[np.where(mask)] = 0
+        roi += logo
+
         # Write frames.
         video_output.write(image)
 
         # Display.
-        cv2.imshow('MediaPipe Pose', image)
+        cv2.imshow('HealBone Pose', image)
         if cv2.waitKey(5) & 0xFF == ord('q'):
             break
 
